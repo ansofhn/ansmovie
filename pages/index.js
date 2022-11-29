568a0842f3db55f21d9e975bfdc032ad78888173
Ansof Habibunnadjar <ansofhn5@gmail.com>
2022-11-29 18:15:51 +0700

    feat: build search movie n wiring api movieDB

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
+import { getMovieList, searchMovie } from "../repository/api";
 
-export default function Home() {
+const Home = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result);
+    });
+  }, []);
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className="text-black bg-gray-300 w-[400px]" key={i}>
+          <div className="">{movie.title}</div>
+          <img
+            className="h-full"
+            src={`${process.env.NEXT_PUBLIC_BASEIMGURL}${movie.poster_path}`}
+          />
+          <div className="">{movie.release_date}</div>
+          <div className="">{movie.vote_average}</div>
+        </div>
+      );
+    });
+  };
+
+  const search = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q);
+      setPopularMovies(query);
+    }
+  };
+
+  console.log({ popularMovies: popularMovies });
+
   return (
-    <>
+    <div className="bg-black">
       <Head>
         <title>Home | AnsMovie</title>
       </Head>
-    </>
+      <div className="py-5 text-4xl font-bold text-center text-gray-100 uppercase">
+        ansmovie
+      </div>
+      <div className="text-center">
+        <input
+          placeholder="Search film.."
+          className="p-2 my-2 bg-gray-100"
+          onChange={({ target }) => search(target.value)}
+        />
+        <div className="flex flex-wrap items-center justify-center gap-5">
+          <PopularMovieList />
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default Home;
---
+++ repository/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+const apiKey = process.env.NEXT_PUBLIC_APIKEY;
+
+export const getMovieList = async () => {
+  const movie = await axios.get(`${baseUrl}/movie/popular?page=1&api_key=${apiKey}`);
+  return movie.data.results;
+};
+
+export const searchMovie = async (q) => {
+  const search = await axios.get(
+    `${baseUrl}/search/movie?page=1&query=${q}&api_key=${apiKey}`
+  );
+  return search.data.results
+};
