721ed40fa3957748daf633c20590937623488e08
Ansof Habibunnadjar <ansofhn5@gmail.com>
2022-12-08 14:44:07 +0700

    feat: add appearance homepage

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import Navbar from "../components/Navbar";
 import ScrollToTop from "../components/ScrollToTop";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
+import { AiFillLike } from "react-icons/ai";
+import { BiCameraMovie, BiMoviePlay } from "react-icons/bi";
 
 const Home = () => {
   const [popularMovies, setPopularMovies] = useState([]);
@@ -18,14 +20,27 @@ const Home = () => {
   const PopularMovieList = () => {
     return popularMovies.map((movie, i) => {
       return (
-        <div className="text-black bg-gray-300 w-[400px]" key={i}>
-          <div className="">{movie.title}</div>
-          <img
-            className="h-full"
-            src={`${process.env.NEXT_PUBLIC_BASEIMGURL}${movie.poster_path}`}
-          />
-          <div className="">{movie.release_date}</div>
-          <div className="">{movie.vote_average}</div>
+        <div className="pb-10 text-white xl:w-72 2xl:w-80" key={i}>
+          <div className="overflow-hidden rounded-lg ">
+            <img
+              className="h-full"
+              src={`${process.env.NEXT_PUBLIC_BASEIMGURL}${movie.poster_path}`}
+            />
+          </div>
+          <div className="px-1 py-4">
+            <div className="flex items-center h-10 font-semibold">
+              {movie.title}
+            </div>
+          </div>
+          <div className="flex items-center justify-between px-1">
+            <div className="text-xs font-medium text-softCream">
+              {movie.release_date}
+            </div>
+            <div className="flex items-center gap-2 text-xs font-medium">
+              <AiFillLike className="text-sm text-softCream" />
+              {movie.vote_average}
+            </div>
+          </div>
         </div>
       );
     });
@@ -48,24 +63,113 @@ const Home = () => {
 
       {/* Appearance Website*/}
 
-      <Navbar />
+      <Navbar onSearch={({ target }) => search(target.value)} />
       <Hero />
       <ScrollToTop />
 
-      {/* Appearance Website*/}
+      {/* Popular Movies */}
 
-      <div className="py-20 text-center bg-black/20">
-        <input
-          placeholder="Search film.."
-          className="p-2 my-2 bg-gray-100"
-          onChange={({ target }) => search(target.value)}
-        />
-        <div className="flex flex-wrap items-center justify-center gap-5">
-          <PopularMovieList />
+      <div className="py-24 bg-black/20">
+        <div className="container space-y-10">
+          <div className="grid grid-cols-2">
+            <div className="space-y-2">
+              <div className="text-xs font-semibold tracking-widest uppercase text-softCream">
+                Online Streaming
+              </div>
+              <div className="text-4xl font-bold text-white">
+                Popular Movies
+              </div>
+            </div>
+            <div className="flex items-end justify-end">
+              <div className="px-3 py-2 text-xs font-medium text-white transition duration-300 border-2 cursor-pointer border-softCream rounded-2xl sm:py-2 sm:text-sm sm:px-10 bg-backGround">
+                Movies
+              </div>
+              <div className="px-3 py-2 ml-3 text-xs font-medium text-white transition duration-300 border-2 cursor-pointer border-softCream rounded-2xl sm:py-2 sm:text-sm sm:px-10 bg-backGround">
+                TV Show
+              </div>
+              <div></div>
+            </div>
+          </div>
+
+          <div className="flex flex-wrap items-center justify-center gap-8 pt-6 2xl:gap-16">
+            <PopularMovieList />
+          </div>
         </div>
       </div>
 
-      <Footer />
+      {/* Our Service */}
+
+      <div className="h-[80vh] bg-backGround flex items-center">
+        <div className="container grid grid-cols-2">
+          <div>asdf</div>
+          <div className="flex items-center text-white">
+            <div className="space-y-4">
+              <div className="text-xs font-semibold tracking-widest uppercase text-softCream">
+                Our Services
+              </div>
+              <div className="w-5/6 text-4xl font-bold text-white">
+                Download Your Movies Watch Offline.
+              </div>
+              <div>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. LoremIpsumhas been the industry's standard dummy text
+                ever since the 1500s. adskf;aimsdfjas
+                ;dlkfiasd;flaksd;flka.dfgsdfgsdfgs
+              </div>
+              <div className="grid w-5/6 grid-cols-6 gap-4 pt-6">
+                <div className="flex items-center justify-center col-span-2">
+                  <div className="p-4 text-5xl text-white border-2 rounded-full border-softCream">
+                    <BiMoviePlay />
+                  </div>
+                </div>
+                <div className="col-span-4 col-start-3 my-auto space-y-3">
+                  <div className="text-lg font-bold">Enjoy on Your Movie.</div>
+                  <div>ljasdlfkjalsdfkjalskdjflaskjdfl</div>
+                </div>
+                <div className="flex items-center justify-center col-span-2 row-start-3">
+                  <div className="p-4 text-5xl text-white border-2 rounded-full border-softCream">
+                    <BiCameraMovie />
+                  </div>
+                </div>
+                <div className="col-span-4 col-start-3 row-start-3 my-auto space-y-3">
+                  <div className="text-lg font-bold ">Watch Everywhere.</div>
+                  <div>lkdsajflsdjkflajsdlfjalsdkjfla</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Top Rated Movies */}
+
+      <div className="py-24 bg-black/20">
+        <div className="container">
+          <div className="space-y-2 text-center">
+            <div className="text-xs font-semibold tracking-widest uppercase text-softCream">
+              Online Streaming
+            </div>
+            <div className="text-4xl font-bold text-white">
+              Top Rated Movies
+            </div>
+          </div>
+          <div className="flex items-center justify-center py-10">
+            <div className="px-3 py-2 text-xs font-medium text-white transition duration-300 rounded-sm cursor-pointer border-x-2 border-x-softCream sm:py-2 sm:text-sm sm:px-10 bg-backGround">
+              Movies
+            </div>
+            <div className="px-3 py-2 ml-3 text-xs font-medium text-white transition duration-300 rounded-sm cursor-pointer border-x-2 border-x-softCream sm:py-2 sm:text-sm sm:px-10 bg-backGround">
+              TV Show
+            </div>
+          </div>
+
+          <div className="flex flex-wrap items-center justify-center gap-8 pt-6 2xl:gap-16">
+            <PopularMovieList />
+          </div>
+
+        </div>
+      </div>
+
+      <Footer onSearch={({ target }) => search(target.value)} />
     </div>
   );
 };
