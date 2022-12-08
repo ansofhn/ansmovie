53a96490dc29ed4d038c12339ea1a586e4816c0d
Ansof Habibunnadjar <ansofhn5@gmail.com>
2022-12-08 17:47:42 +0700

    fix: build

--- components/Hero.js
+++ components/Hero.js
@@ -1,17 +1,10 @@
-import Image from "next/image";
 import React from "react";
-import Logo from "../public/assets/logo.svg";
 
 const Hero = () => {
   return (
     <div className="bg-backGround">
       <div className="container flex items-center w-screen h-screen lg:h-[95vh] lg:pt-10">
         <div className="grid gap-4 mx-auto lg:grid-cols-2 lg:px-8 xl:px-0">
-          {/* <div className="flex items-center justify-center text-white lg:justify-end lg:order-1">
-            <div className="w-full h-full p-10 lg:w-[95%] xl:w-[90%] 2xl:w-[85%] lg:h-[135%] border-4 rounded-md border-softCream">
-              Ini gambar
-            </div>
-          </div> */}
           <div className="my-auto space-y-4">
             <div className="text-xl font-bold text-softCream">Movsoft</div>
             <div className="text-4xl font-bold text-white md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl">
