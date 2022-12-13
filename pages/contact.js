b5113c09a09eadfef7e8f030cd743580e71758ae
Ansof Habibunnadjar <ansofhn5@gmail.com>
2022-12-13 11:38:17 +0700

    feat: add page about, contact, n movielist

---
+++ pages/contact.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import ScrollToTop from "../components/ScrollToTop";
+
+const contact = () => {
+  return (
+    <div>
+      <Navbar />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Footer />
+      <ScrollToTop />
+    </div>
+  );
+};
+
+export default contact;
