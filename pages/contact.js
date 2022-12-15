7451c9e7411fa49cf6b5a0add93a5752a14270ef
Ansof Habibunnadjar <ansofhn5@gmail.com>
2022-12-15 16:18:39 +0700

    feat: add content pages contact n movie

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,21 @@
 import React from "react";
+import Callback from "../components/Callback";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import ScrollToTop from "../components/ScrollToTop";
 
 const contact = () => {
   return (
-    <div>
+    <div className="bg-backGround">
+      <div className="pb-40 text-center pt-60 bg-black/20">
+        <div className="text-4xl font-bold text-white md:text-6xl">
+          Contact Us
+        </div>
+        <div className="text-sm font-semibold text-white uppercase">
+          <span className="mr-2 text-softCream">Home</span> |{" "}
+          <span className="ml-2">Contact</span>
+        </div>
+      </div>
       <Navbar />
       <br />
       <br />
@@ -20,6 +30,7 @@ const contact = () => {
       <br />
       <br />
       <br />
+      <Callback />
       <Footer />
       <ScrollToTop />
     </div>
--- pages/movielist.js
+++ pages/movielist.js
@@ -1,11 +1,21 @@
 import React from "react";
+import Callback from "../components/Callback";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import ScrollToTop from "../components/ScrollToTop";
 
 const movielist = () => {
   return (
-    <div>
+    <div className="bg-backGround">
+      <div className="pb-40 text-center pt-60 bg-black/20">
+        <div className="text-4xl font-bold text-white md:text-6xl">
+          Our Movie
+        </div>
+        <div className="text-sm font-semibold text-white uppercase">
+          <span className="mr-2 text-softCream">Home</span> |{" "}
+          <span className="ml-2">Movie</span>
+        </div>
+      </div>
       <Navbar />
       <br />
       <br />
@@ -20,6 +30,7 @@ const movielist = () => {
       <br />
       <br />
       <br />
+      <Callback />
       <Footer />
       <ScrollToTop />
     </div>
